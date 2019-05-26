195bb95fd5772e6e467d4da99eae820aa3e9cd62
Neyda25 <48398873+Neyda25@users.noreply.github.com>
2019-05-25 23:26:44 -0600

    Add files via upload

---
+++ multiplicacion1.py
@@ -0,0 +1,24 @@
+print("Tabla del 2")
+for elemento in range(1,11):
+	resultado = 2*elemento
+	print("2x{0}={1}", format(elemto,resultado))
+print()
+
+print("Tabla del 4")
+for elemento in range(1,11):
+	resultado = 4*elemento
+	print("4x{0}={1}", format(elemto,resultado))
+print()
+
+print("Tabla del 23")
+for elemento in range(1,11):
+	resultado = 23*elemento
+	print("23x{0}={1}", format(elemto,resultado))
+print()
+
+print("Tabla del 76")
+for elemento in range(1,11):
+	resultado = 76*elemento
+	print("76x{0}={1}", format(elemto,resultado))
+print()
+
---
+++ multiplicacion2.py
@@ -0,0 +1,18 @@
+for indice in range (32,36):
+	print("Tabla del ", indice)
+	for elemento in range(1,11)
+		resultado = indice*elemento
+		print("{2} x {0} = {1}".format(elemnto,resultado,indice))
+	print()
+	
+print()
+print("otros valores")
+print()
+	
+tablas= [21, 34, 54, 65,76]
+for indicedice in tablas:
+	print("Tablas del", indice)
+	for elemento in range(1,11):
+		resultado = indice*elemnto
+		print("{2} x {0} = {1}".format(elemnto,resultado,indice))
+	print() 
